d8baf79101f5f84dca2265ac1dec7c780cffa2f2
10sa <zenkins26@gmail.com>
2016-07-06 01:16:41 +0900

    Fix 1

--- 2016_KOI/Properties/Resources.Designer.cs
+++ 2016_KOI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace _2016_KOI.Properties {
+namespace Form.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace _2016_KOI.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("_2016_KOI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Form.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- 2016_KOI/Properties/Settings.Designer.cs
+++ 2016_KOI/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace _2016_KOI.Properties {
+namespace Form.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]

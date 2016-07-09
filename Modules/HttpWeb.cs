66c28b5924a5992bb2d2cc2ff0ce350d3855042b
Tensa <zenkins26@gmail.com>
2016-07-09 10:50:18 +0900

    네임스페이스 수정.

--- Modules/HttpWeb.cs
+++ Modules/HttpWeb.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Framework;
+using Framework.Module.Base;
 
 namespace Modules.HttpWeb
 {
